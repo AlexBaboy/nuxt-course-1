5f9b69d1e865de083e31c409fecefda0fac4ce29
alex baboy <alexbaboy2016@gmail.com>
2021-08-30 23:22:09 +0300

    route + vuex

--- pages/users/index.vue
+++ pages/users/index.vue
@@ -1,13 +1,27 @@
 <template>
   <div>
     <h1>Пользователи</h1>
+    <ul>
+      <li
+        v-for="user in users"
+        :key="user.id"
+      >
+        <nuxt-link :to="`users/${user.id}`">{{ user.name }}</nuxt-link>
+      </li>
+    </ul>
     <nuxt-link to="/users/1">Перейти к пользователю</nuxt-link>
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 export default {
-  name: 'users'
+  name: 'users',
+  computed: {
+    ...mapGetters({
+      users: 'users/getUsers'
+    })
+  }
 }
 </script>
 
---
+++ store/users.js
@@ -0,0 +1,20 @@
+export const state = () => ({
+  users: [
+    {
+      id: 1,
+      name: 'User 1'
+    },
+    {
+      id: 2,
+      name: 'User 2'
+    },
+    {
+      id: 3,
+      name: 'User 3'
+    }
+  ]
+})
+
+export const getters = {
+  getUsers: state => state.users
+}
